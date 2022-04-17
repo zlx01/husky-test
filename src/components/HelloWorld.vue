79fe0cef99f80956f92db032448c299b0ecaaa45
zlx01 <761451281@qq.com>
2022-04-17 17:38:38 +0800

    提交之前会执行npm run lint

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,6 +10,7 @@ defineProps({
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
+    <h2>hahaha</h2>
     <h3>
       You’ve successfully created a project with
       <a target="_blank" href="https://vitejs.dev/">Vite</a> +
